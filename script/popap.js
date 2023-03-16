bc7e16c9c1e8eadd0217f24282b1e22a4a70d508
akademuk <akademuk24@gmail.com>
2023-03-16 11:08:22 +0200

    sss

--- script/popap.js
+++ script/popap.js
@@ -10,3 +10,23 @@ function hidePopup() {
 
 document.getElementById("btn-open-popup").addEventListener("click", showPopup);
 document.getElementById("btn-close-popup").addEventListener("click", hidePopup);
+
+
+
+// Находим элементы на странице
+var myModalCredit = document.getElementById("myModalCredit");
+var btn = document.getElementById("myModalCreditBtn");
+var span = document.getElementsByClassName("myModalCreditClose")[0];
+
+// Когда пользователь кликает на кнопку, открываем popup
+btn.onclick = function() {
+  myModalCredit.style.display = "flex";
+}
+
+// Когда пользователь кликает на "x", закрываем popup
+span.onclick = function() {
+  myModalCredit.style.display = "none";
+}
+
+
+
